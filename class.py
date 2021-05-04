b92f2ee6937f0ad69bba69888e477497fb5e6084
Ваше Имя <you@example.com>
2021-05-04 19:32:57 +0600

    dfasf

---
+++ class.py
@@ -0,0 +1,13 @@
+class Jet
+    name = "falcon"
+    model = 'F-35'
+
+    # создаем методы класса
+    def country(self):
+        print ("USA")
+
+
+# Создаем объект класса Car под названием car_a
+car_a = Car()
+# Создаем объект класса Car под названием car_b
+car_b = Car()
